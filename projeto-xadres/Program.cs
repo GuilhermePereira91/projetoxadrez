c0d6c53d477001f10edb183e92efbdf1079a36f5
GuilhermePereira91 <guilhermexmarques91@gmail.com>
2020-12-06 20:02:38 -0300

    Projeto criado

---
+++ projeto-xadres/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace projeto_xadres
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
