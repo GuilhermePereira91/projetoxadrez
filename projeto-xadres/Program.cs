83354555d2fc04cda553f1d2f96bceb7042660b8
GuilhermePereira91 <guilhermexmarques91@gmail.com>
2020-12-06 21:32:32 -0300

    Melhorando a implementação do metodo Imprimir tabuleiro

--- projeto-xadres/Program.cs
+++ projeto-xadres/Program.cs
@@ -10,10 +10,15 @@ namespace projeto_xadres
         {
             try
             {
-                PosicaoXadrez pos = new PosicaoXadrez('c', 7);
-                Console.WriteLine(pos);
+                Tabuleiro tab = new Tabuleiro(8, 8);
+                tab.colocarPeca(new Torre(tab, Cor.Preta), new Posicao(0, 0));
+                tab.colocarPeca(new Torre(tab, Cor.Preta), new Posicao(1, 3));
+                tab.colocarPeca(new Rei(tab, Cor.Preta), new Posicao(0, 2));
 
-                Console.WriteLine(pos.toPosicao());
+                tab.colocarPeca(new Rei(tab, Cor.Branca), new Posicao(0, 1));
+                tab.colocarPeca(new Torre(tab, Cor.Branca), new Posicao(1, 4));
+                tab.colocarPeca(new Rei(tab, Cor.Branca), new Posicao(1, 2));
+                Tela.imprimirTabuleiro(tab);
             }catch(TabuleiroException e)
             {
                 Console.WriteLine(e.Message);
