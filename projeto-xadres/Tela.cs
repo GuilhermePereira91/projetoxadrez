3c8298f5fe8eb2cf24a8dd10b9b9d1b810e336f6
GuilhermePereira91 <guilhermexmarques91@gmail.com>
2020-12-27 16:10:45 -0300

    Implementando a verificação de xeque

--- projeto-xadres/Tela.cs
+++ projeto-xadres/Tela.cs
@@ -15,6 +15,10 @@ namespace projeto_xadres
             Console.WriteLine();
             Console.WriteLine("Turno: " + partida.turno);
             Console.WriteLine("Aguardando jogada: " + partida.JogadorAtual);
+            if (partida.xeque)
+            {
+                Console.WriteLine("XEQUE!");
+            }
         }
         public static void imprimirPecasCapturadas(PartidaDeXadrez partida)
         {
